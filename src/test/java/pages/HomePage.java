b2f3746ba484a34ef5c5422559a9d0e0f565d156
maja27 <maja.27@yahoo.com>
2019-12-23 13:46:52 +0100

    update login with valid credentials

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,4 +1,16 @@
 package pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class HomePage {
+    WebDriver driver;
+    By signIn = By.xpath("//a[@class='account']");
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getProfileName() {
+        return driver.findElement(signIn).getText();
+    }
 }
--- src/test/java/tests/MaLoginWithValidCredentials1.java
+++ src/test/java/tests/MaLoginWithValidCredentials1.java
@@ -1,16 +1,19 @@
 package tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 import pages.LoginPage;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
 
 public class MaLoginWithValidCredentials1 {
     String basePath = "http://automationpractice.com";
     WebDriver driver = new ChromeDriver();
     LoginPage loginPage = new LoginPage(driver);
+    HomePage homePage = new HomePage(driver);
 
     @BeforeTest
     public void setUp() {
@@ -25,7 +28,10 @@ public class MaLoginWithValidCredentials1 {
         loginPage.setEmail("maja.27@yahoo.com");
         loginPage.setPassword("caocaocao");
         loginPage.clickLogin();
+        assertEquals(homePage.getProfileName(), "Maja Andjelkovic");
     }
+
+
 
     @AfterTest
     public void closeApp() {
